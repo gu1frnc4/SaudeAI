3cce4a18002072e5fb2ad85a1bf0ba9cc9b1ca93
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2024-11-24 15:03:52 -0300

    Criação das rotas das funções de login, registro e mudança de senha

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { AuthComponent } from './views/auth/auth.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Página inicial
+  { path: 'login', component: AuthComponent }, // Tela de login
+  { path: 'register', component: AuthComponent }, // Tela de registro
+  { path: 'forgot-password', component: AuthComponent }, // Recuperação de senha
 ];
