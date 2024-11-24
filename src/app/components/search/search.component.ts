4da97a8d94741fa608626d7360bcf7fcb1b61170
alvesjaov <victoralves169@outlook.com>
2024-11-24 12:05:25 -0300

    Adiciona melhorias ao componente de busca: implementa rolagem para resultados, botão de detalhes e validação de entrada; atualiza estilos e lógica do modal

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
     imports: [CommonModule, FormsModule, HttpClientModule],
@@ -17,10 +18,10 @@ export class SearchComponent {
     showOverlay: boolean = false; // Adicionado para controlar a exibição do modal
     showWarning: boolean = false; // Adicionado para controlar a exibição da mensagem de aviso
 
-    constructor(private readonly http: HttpClient) {}
+    constructor(private readonly http: HttpClient, private router: Router) {}
 
     onSearch() {
-        if (!this.city.trim()) {
+        if (!this.city.trim() && !this.state.trim() && !this.region.trim()) {
             this.showWarning = true;
             return;
         }
@@ -30,9 +31,9 @@ export class SearchComponent {
         this.http.get<any[]>(url).subscribe(data => {
             console.log(data); // Adicionado para depuração
             this.searchResults = data.filter(item => 
-                item.nome.toLowerCase().includes(this.city.toLowerCase()) &&
-                (!this.state || item.estado.toLowerCase().includes(this.state.toLowerCase())) &&
-                (!this.region || item.regiao.toLowerCase().includes(this.region.toLowerCase()))
+                (!this.city || this.removeAccents(item.nome).toLowerCase().includes(this.removeAccents(this.city).toLowerCase())) &&
+                (!this.state || this.removeAccents(item.estado).toLowerCase().includes(this.removeAccents(this.state).toLowerCase())) &&
+                (!this.region || this.removeAccents(item.regiao).toLowerCase().includes(this.removeAccents(this.region).toLowerCase()))
             );
             this.showOverlay = true; // Mostrar o modal após a pesquisa
         });
@@ -40,5 +41,13 @@ export class SearchComponent {
 
     closeModal() {
         this.showOverlay = false; // Fechar o modal
+    }
+
+    viewDetails(id: number) {
+        this.router.navigate(['/details', id]);
+    }
+
+    removeAccents(str: string): string {
+        return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
     }
 }
